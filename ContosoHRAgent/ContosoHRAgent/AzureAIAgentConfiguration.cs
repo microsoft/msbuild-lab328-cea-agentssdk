2e679171d706d36c431ce50358999ada5a50e851
Ayca Bas <36196437+aycabas@users.noreply.github.com>
2025-05-07 16:34:45 +0400

    agent adede

---
+++ ContosoHRAgent/ContosoHRAgent/AzureAIAgentConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace ContosoHRAgent
+{
+    public class AzureAIAgentConfiguration
+    {
+        public string AgentId { get; set; } = string.Empty;
+
+        public string ConnectionString { get; set; } = string.Empty;
+    }
+}
